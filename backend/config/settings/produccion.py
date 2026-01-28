f17710ef0cf70fb0b14f990c28f6c754a17b2296
Juan Manuel Cruz P. <juanma@MacBook-Air-de-Juan-2.local>
2026-01-28 11:41:23 -0500

    ajustes

--- backend/config/settings/produccion.py
+++ backend/config/settings/produccion.py
@@ -1,17 +1,16 @@
-from .base import *  # noqa: F403
-import sentry_sdk
-from sentry_sdk.integrations.django import DjangoIntegration
+from .base import *  # noqa: F403, F401
 
 DEBUG = False
+ALLOWED_HOSTS = ['64.227.110.12', 'confeccionesunoa.com']
 
 
 DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.postgresql_psycopg2',
-        'NAME': 'base-apps',
-        'USER': 'user-prod',
-        'PASSWORD': 'pass-prod',
-        'HOST': 'ip-host-prod',
+        'NAME': 'confecciones',
+        'USER': 'confecciones',
+        'PASSWORD': 'Confecciones2026!',
+        'HOST': '64.227.110.12',
         'PORT': '5432',
     }
 }
@@ -19,19 +18,3 @@ DATABASES = {
 
 MEDIA_ROOT = '/media/'  # Docker volume
 STATIC_ROOT = "/public/"  # Docker volume
-
-sentry_sdk.init(
-    dsn="http://d0393edd210348698c87134d0c3dc6fd@20.102.86.65:9000/8",
-    integrations=[DjangoIntegration()],
-    environment="produccion",
-
-    # Set traces_sample_rate to 1.0 to capture 100%
-    # of transactions for performance monitoring.
-    # We recommend adjusting this value in production.
-    traces_sample_rate=1.0,
-
-    # If you wish to associate users to errors (assuming you are using
-    # django.contrib.auth) you may enable sending PII data.
-    send_default_pii=True,
-    release=os.environ.get('GIT_COMMIT_SHA', 'release_desconocido')  # noqa: F405
-)
